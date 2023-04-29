a2953f3ef10ec90c86f85c872de7c777eaef5470
Yahirs19 <L21212055@tectijuana.edu.mx>
2023-04-29 13:44:37 -0700

    x

--- Proyecto Base de Datos/Form1.Designer.cs
+++ Proyecto Base de Datos/Form1.Designer.cs
@@ -36,10 +36,10 @@
             this.panelTop = new System.Windows.Forms.Panel();
             this.ibChildForm = new FontAwesome.Sharp.IconButton();
             this.panelDesktop = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.panelMenu.SuspendLayout();
             this.panelTop.SuspendLayout();
             this.panelDesktop.SuspendLayout();
@@ -54,9 +54,8 @@
             this.panelMenu.Controls.Add(this.label1);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
-            this.panelMenu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(248, 779);
+            this.panelMenu.Size = new System.Drawing.Size(220, 623);
             this.panelMenu.TabIndex = 0;
             // 
             // iconButton3
@@ -70,10 +69,10 @@
             this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton3.IconSize = 36;
             this.iconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton3.Location = new System.Drawing.Point(0, 180);
+            this.iconButton3.Location = new System.Drawing.Point(0, 146);
             this.iconButton3.Margin = new System.Windows.Forms.Padding(0);
             this.iconButton3.Name = "iconButton3";
-            this.iconButton3.Size = new System.Drawing.Size(248, 75);
+            this.iconButton3.Size = new System.Drawing.Size(220, 60);
             this.iconButton3.TabIndex = 3;
             this.iconButton3.Text = "SALIR";
             this.iconButton3.UseVisualStyleBackColor = false;
@@ -90,10 +89,10 @@
             this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton2.IconSize = 36;
             this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton2.Location = new System.Drawing.Point(0, 105);
+            this.iconButton2.Location = new System.Drawing.Point(0, 86);
             this.iconButton2.Margin = new System.Windows.Forms.Padding(0);
             this.iconButton2.Name = "iconButton2";
-            this.iconButton2.Size = new System.Drawing.Size(248, 75);
+            this.iconButton2.Size = new System.Drawing.Size(220, 60);
             this.iconButton2.TabIndex = 2;
             this.iconButton2.Text = "Gestion de Recibos";
             this.iconButton2.UseVisualStyleBackColor = false;
@@ -110,10 +109,10 @@
             this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton1.IconSize = 36;
             this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.iconButton1.Location = new System.Drawing.Point(0, 30);
+            this.iconButton1.Location = new System.Drawing.Point(0, 26);
             this.iconButton1.Margin = new System.Windows.Forms.Padding(0);
             this.iconButton1.Name = "iconButton1";
-            this.iconButton1.Size = new System.Drawing.Size(248, 75);
+            this.iconButton1.Size = new System.Drawing.Size(220, 60);
             this.iconButton1.TabIndex = 1;
             this.iconButton1.Text = "Home";
             this.iconButton1.UseVisualStyleBackColor = false;
@@ -127,7 +126,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(241, 30);
+            this.label1.Size = new System.Drawing.Size(206, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Gestion De Recibos";
             // 
@@ -135,10 +134,9 @@
             // 
             this.panelTop.Controls.Add(this.ibChildForm);
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Location = new System.Drawing.Point(248, 0);
-            this.panelTop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelTop.Location = new System.Drawing.Point(220, 0);
             this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(1082, 100);
+            this.panelTop.Size = new System.Drawing.Size(962, 80);
             this.panelTop.TabIndex = 1;
             // 
             // ibChildForm
@@ -155,7 +153,7 @@
             this.ibChildForm.Location = new System.Drawing.Point(0, 0);
             this.ibChildForm.Margin = new System.Windows.Forms.Padding(0);
             this.ibChildForm.Name = "ibChildForm";
-            this.ibChildForm.Size = new System.Drawing.Size(248, 100);
+            this.ibChildForm.Size = new System.Drawing.Size(220, 80);
             this.ibChildForm.TabIndex = 2;
             this.ibChildForm.Text = "Home";
             this.ibChildForm.UseVisualStyleBackColor = false;
@@ -167,18 +165,26 @@
             this.panelDesktop.Controls.Add(this.label3);
             this.panelDesktop.Controls.Add(this.label2);
             this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelDesktop.Location = new System.Drawing.Point(248, 100);
-            this.panelDesktop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelDesktop.Location = new System.Drawing.Point(220, 80);
             this.panelDesktop.Name = "panelDesktop";
-            this.panelDesktop.Size = new System.Drawing.Size(1082, 679);
+            this.panelDesktop.Size = new System.Drawing.Size(962, 543);
             this.panelDesktop.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(174, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Kemish imbecil";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(275, 103);
+            this.label4.Location = new System.Drawing.Point(244, 82);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(153, 20);
+            this.label4.Size = new System.Drawing.Size(133, 16);
             this.label4.TabIndex = 2;
             this.label4.Text = "labelkemish pendejo";
             // 
@@ -186,39 +192,29 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label3.Location = new System.Drawing.Point(395, 360);
+            this.label3.Location = new System.Drawing.Point(351, 288);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(154, 22);
+            this.label3.Size = new System.Drawing.Size(132, 18);
             this.label3.TabIndex = 1;
             this.label3.Text = "El jemish se la come";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(387, 201);
+            this.label2.Location = new System.Drawing.Point(344, 161);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.Size = new System.Drawing.Size(83, 16);
             this.label2.TabIndex = 0;
             this.label2.Text = "yahor guapo";
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(196, 201);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(113, 20);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Kemish imbecil";
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1330, 779);
+            this.ClientSize = new System.Drawing.Size(1182, 623);
             this.Controls.Add(this.panelDesktop);
             this.Controls.Add(this.panelTop);
             this.Controls.Add(this.panelMenu);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
